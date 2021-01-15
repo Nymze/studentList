f26e6d835a4fdfe837e6352ad8e06ef9ab66bbd5
Arnaud Mnd <31897151+Nymze@users.noreply.github.com>
2021-01-15 13:46:45 +0100

    changed exemple

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../example";
+import App from "./example";
 
 ReactDOM.render(<App />, document.getElementById("root"));
